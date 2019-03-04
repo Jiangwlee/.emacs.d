84a686ffd9c593a302b11790b888891f607d5e2a
Bruce <jiangwl@synopsys.com>
2019-03-04 16:14:09 +0800

    Initial Version

---
+++ init.el
@@ -0,0 +1,120 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-company-mode t)
+ '(global-linum-mode t)
+ '(org-agenda-files (quote ("~/Git/org-mode/agenda.org")))
+ '(package-selected-packages
+   (quote
+    (org ace-window spaceline-all-the-icons spaceline counsel swiper ace-jump-mode ivy company projectile neotree evil-magit magit dracula-theme color-theme evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Evil configuration
+(setq evil-want-C-u-scroll t)
+(require 'evil)
+(evil-mode 1)
+
+;; NeoTree
+(setq neo-smart-open t)
+(add-hook 'neotree-mode-hook
+    (lambda ()
+    (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+    (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+    (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+    (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+    (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+    (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
+
+;; Color theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; Ivy mode
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t) ;; add 'recentf-mode' and bookmarks to 'ivy-switch-buffer'
+(setq ivy-count-format "(%d/%d) ")
+(setq ivy-height 15) ;; number of result line to display
+(setq ivy-fixed-height-minibuffer t)
+;;(setq ivy-count-format "") ;; does not count candidates
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;;(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;;(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;;(global-set-key (kbd "<f1> l") 'counsel-find-library)
+;;(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+
+;; Projectile
+(require 'projectile)
+(setq projectile-completion-system 'ivy)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+(projectile-global-mode)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; Company
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Linenum
+(setq linum-format "%4d \u2502 ")
+
+;; magit
+(global-set-key (kbd "C-c m") 'magit-status)
+(setq ediff-diff-options "-w"
+      ediff-split-window-function 'split-window-horizontally
+      ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+
+;; menu bar
+(menu-bar-mode -1)
+
+;; spaceline
+(spaceline-emacs-theme)
+(spaceline-highlight-face-evil-state)
+
+;; ace-window
+(global-set-key (kbd "M-o") 'ace-window)
+
+;; org-mode
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-todo-keywords '((sequence "TODO" "DOING" "DONE")))
